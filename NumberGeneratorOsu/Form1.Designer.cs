4feb39db0fdab49d74e3cb78430eeee4bd87f629
avail <avail@pomf.se>
2015-07-19 14:33:02 +0200

    Working!

---
+++ NumberGeneratorOsu/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace NumberGeneratorOsu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this._fontTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this._checkBox2x = new System.Windows.Forms.CheckBox();
+            this._buttonGenerate = new System.Windows.Forms.Button();
+            this._buttonBrowse = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this._buttonColor = new System.Windows.Forms.Button();
+            this._comboBoxType = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // _fontTextBox
+            // 
+            this._fontTextBox.Enabled = false;
+            this._fontTextBox.Location = new System.Drawing.Point(56, 48);
+            this._fontTextBox.Name = "_fontTextBox";
+            this._fontTextBox.Size = new System.Drawing.Size(121, 20);
+            this._fontTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Font:";
+            // 
+            // _checkBox2x
+            // 
+            this._checkBox2x.AutoSize = true;
+            this._checkBox2x.Location = new System.Drawing.Point(56, 74);
+            this._checkBox2x.Name = "_checkBox2x";
+            this._checkBox2x.Size = new System.Drawing.Size(93, 17);
+            this._checkBox2x.TabIndex = 2;
+            this._checkBox2x.Text = "generate @2x";
+            this._checkBox2x.UseVisualStyleBackColor = true;
+            // 
+            // _buttonGenerate
+            // 
+            this._buttonGenerate.Location = new System.Drawing.Point(264, 20);
+            this._buttonGenerate.Name = "_buttonGenerate";
+            this._buttonGenerate.Size = new System.Drawing.Size(75, 49);
+            this._buttonGenerate.TabIndex = 3;
+            this._buttonGenerate.Text = "GENERATE";
+            this._buttonGenerate.UseVisualStyleBackColor = true;
+            this._buttonGenerate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // _buttonBrowse
+            // 
+            this._buttonBrowse.Location = new System.Drawing.Point(183, 46);
+            this._buttonBrowse.Name = "_buttonBrowse";
+            this._buttonBrowse.Size = new System.Drawing.Size(75, 23);
+            this._buttonBrowse.TabIndex = 4;
+            this._buttonBrowse.Text = "Browse";
+            this._buttonBrowse.UseVisualStyleBackColor = true;
+            this._buttonBrowse.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // _buttonColor
+            // 
+            this._buttonColor.Location = new System.Drawing.Point(183, 20);
+            this._buttonColor.Name = "_buttonColor";
+            this._buttonColor.Size = new System.Drawing.Size(75, 23);
+            this._buttonColor.TabIndex = 5;
+            this._buttonColor.Text = "Color";
+            this._buttonColor.UseVisualStyleBackColor = true;
+            this._buttonColor.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // _comboBoxType
+            // 
+            this._comboBoxType.FormattingEnabled = true;
+            this._comboBoxType.Items.AddRange(new object[] {
+            "Default",
+            "Score"});
+            this._comboBoxType.Location = new System.Drawing.Point(56, 21);
+            this._comboBoxType.Name = "_comboBoxType";
+            this._comboBoxType.Size = new System.Drawing.Size(121, 21);
+            this._comboBoxType.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Type:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 102);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this._comboBoxType);
+            this.Controls.Add(this._buttonColor);
+            this.Controls.Add(this._buttonBrowse);
+            this.Controls.Add(this._buttonGenerate);
+            this.Controls.Add(this._checkBox2x);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this._fontTextBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "osu!numgen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox _fontTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox _checkBox2x;
+        private System.Windows.Forms.Button _buttonGenerate;
+        private System.Windows.Forms.Button _buttonBrowse;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button _buttonColor;
+        private System.Windows.Forms.ComboBox _comboBoxType;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ NumberGeneratorOsu/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Drawing.Imaging;
+using SharpFont;
+using System.Runtime.InteropServices;
+using System.IO;
+
+
+namespace NumberGeneratorOsu
+{
+    public partial class Form1 : Form
+    {
+        string file = "";
+        bool isScore = false;
+
+        public void generateStuff(int size = 24, bool _2x = false)
+        {
+            Library font = new Library();
+            Face face = font.NewFace(file, 0);
+            //Stroker stroke = new Stroker(font);
+
+            int height = 0;
+            char averageCh = 'A';
+            double fontScale = 1;
+            string[] characters = new string[] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" };
+
+            if (isScore)
+                characters = new string[] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ".", ",", "%", "x" };
+
+            var entries = new List<FontEntry>();
+
+            face.SetCharSize(0, size << 6, 0, 96);
+            face.SetTransform(new FTMatrix(3 << 16, 0, 0, 3 << 16), FTVector.Unit(0)); // thicknesssss
+
+            //stroke.Set(1, StrokerLineCap.Round, StrokerLineJoin.Round, 0);
+
+            face.LoadGlyph(face.GetCharIndex(averageCh), LoadFlags.Default, LoadTarget.Normal);
+            height = (int)face.Glyph.Metrics.HorizontalBearingY >> 6;
+
+            // then fill the fontentry array
+            foreach (var ch in characters)
+            {
+                face.LoadGlyph(face.GetCharIndex(ch[0]), LoadFlags.Default, LoadTarget.Normal);
+                face.Glyph.RenderGlyph(RenderMode.Normal);
+
+                Bitmap bitmap;
+
+                if (face.Glyph.Bitmap.Width == 0)
+                {
+                    bitmap = new Bitmap(1, 1);
+                }
+                else
+                {
+                    bitmap = new Bitmap(face.Glyph.Bitmap.Width + 4, face.Glyph.Bitmap.Rows + 4);
+
+                    var gg = Graphics.FromImage(bitmap);
+                    gg.Clear(Color.Transparent);
+
+                    var data = bitmap.LockBits(new Rectangle(2, 2, bitmap.Width - 4, bitmap.Height - 4), ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
+
+                    byte[] pixelAlphas = new byte[face.Glyph.Bitmap.Width * face.Glyph.Bitmap.Rows];
+                    Marshal.Copy(face.Glyph.Bitmap.Buffer, pixelAlphas, 0, pixelAlphas.Length);
+
+                    for (int j = 0; j < pixelAlphas.Length; j++)
+                    {
+                        int pixelOffset = (j / data.Width) * data.Stride + (j % data.Width * 4);
+                        Marshal.WriteByte(data.Scan0, pixelOffset + 0, colorDialog1.Color.R);
+                        Marshal.WriteByte(data.Scan0, pixelOffset + 1, colorDialog1.Color.G);
+                        Marshal.WriteByte(data.Scan0, pixelOffset + 2, colorDialog1.Color.B);
+                        Marshal.WriteByte(data.Scan0, pixelOffset + 3, pixelAlphas[j]);
+                    }
+
+                    bitmap.UnlockBits(data);
+                }
+
+                var entry = new FontEntry();
+                entry.Character = ch[0];
+                entry.Bitmap = bitmap;
+                entry.AdvanceX = (int)(((face.Glyph.Metrics.HorizontalAdvance >> 6)) * fontScale);
+                entry.PreX = (int)(((face.Glyph.Metrics.HorizontalBearingX >> 6)) * fontScale);
+                entry.Width = (int)((face.Glyph.Metrics.Width >> 6) * fontScale);
+                entry.Height = (int)((face.Glyph.Metrics.Height >> 6) * fontScale);
+                entry.PreY = (int)((face.Glyph.Metrics.HorizontalBearingY >> 6) * -fontScale);
+
+                if (entry.Width < 2)
+                {
+                    entry.Width = 2;
+                }
+
+                entries.Add(entry);
+            }
+
+            foreach (var entry in entries)
+            {
+                Bitmap bitmap = entry.Bitmap;
+  
+                var fontpath = file.Split('\\').Last().Replace(".ttf", "");
+
+                if (!Directory.Exists(Path.Combine(Directory.GetCurrentDirectory(), "generated\\" + fontpath)))
+                    Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), "generated\\" + fontpath));
+
+                string name = "";
+
+                switch (entry.Character.ToString())
+                {
+                    case ".":
+                        name = "dot";
+                        break;
+
+                    case ",":
+                        name = "comma";
+                        break;
+
+                    case "%":
+                        name = "percent";
+                        break;
+
+                    default:
+                        name = entry.Character.ToString();
+                        break;
+                }
+
+                bitmap.Save(@"generated\\" + fontpath + "\\" + ((isScore) ? "score-" : "default-") + name + ((_2x) ? "@2x" : "") + ".png", ImageFormat.Png);
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            _comboBoxType.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(file))
+                MessageBox.Show("No font file specified", "Shit happened, aboooort");
+
+            if (_comboBoxType.SelectedIndex == 1)
+                isScore = true;
+
+            generateStuff();
+
+            if (_checkBox2x.Checked)
+                generateStuff(42, true);
+
+            MessageBox.Show("The files are waiting for you in the generated/ directory.", "Completed!");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.Filter = "Font file|*.ttf";
+
+            DialogResult OpenFile = openFileDialog1.ShowDialog();
+            if (OpenFile == DialogResult.OK)
+            {
+                file = openFileDialog1.FileName;
+                _fontTextBox.Text = openFileDialog1.FileName.Split('\\').Last().Replace(".ttf", "");
+            }
+            else
+            {
+                Environment.Exit(1);
+            }
+        }
+
+        class FontEntry
+        {
+            public char Character { get; set; }
+            public Bitmap Bitmap { get; set; }
+            public int AdvanceX { get; set; }
+            public int PreY { get; set; }
+            public int Width { get; set; }
+            public int Height { get; set; }
+            public int PreX { get; set; }
+
+            public RectangleF UV { get; set; }
+
+            public override string ToString()
+            {
+                return Character.ToString();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+        }
+    }
+}
